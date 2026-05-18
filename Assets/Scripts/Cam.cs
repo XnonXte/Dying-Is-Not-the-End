706bce7fea615eccba58c8dc034b797109fced7b
johannn29 <johannnesxiimipa315@gmail.com>
2026-05-18 17:01:41 +0700

    bgMainMenu

---
+++ Assets/Scripts/Cam.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MenuCameraLoop : MonoBehaviour
+{
+    public float speed = 2f;
+
+    public float startX;
+    public float endX;
+
+    void Update()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+
+        if (transform.position.x >= endX)
+        {
+            Vector3 pos = transform.position;
+            pos.x = startX;
+
+            transform.position = pos;
+        }
+    }
+}
